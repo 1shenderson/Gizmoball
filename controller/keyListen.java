de4bd359ac04a0f989077be88ee34ba2767b2de3
cchalmers94 <craigch@sky.com>
2015-02-27 21:57:45 +0000

    updated keyListener for certain key input

--- controller/keyListen.java
+++ controller/keyListen.java
@@ -3,8 +3,10 @@ package controller;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+
 //import model.Flipper;
 import model.Model;
+import model.gizmo.FlipperLeft;
 
 public class keyListen implements KeyListener {
 
@@ -13,28 +15,36 @@ public class keyListen implements KeyListener {
 	public keyListen(Model m) {
 		model = m;
 	}
-	
+
 	@Override
 	public void keyPressed(KeyEvent e) {
-	    char c = e.getKeyChar();
-	    System.out.println("Key Typed: " + c);
-	    model.changeFlipperColour();	
-	    model.rotateFlip();
-	
+		char c = e.getKeyChar();
+		if (c == 'l'){
+			model.rotateFlip(true);
+		}
+		if (c == 'r'){
+			model.rotateFlipR(true);
+		}
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-	    char c = e.getKeyChar();
-	   // System.out.println("Key Pressed: " + c);
-				
+		char c = e.getKeyChar();
+		if (c == 'l'){
+			model.rotateFlip(false);
+		}
+		if (c == 'r'){
+			model.rotateFlipR(false);
+		}
+		
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-	    char c = e.getKeyChar();
-	  //  System.out.println("Key Release: " + c);
-				
+		char c = e.getKeyChar();
+		//  System.out.println("Key Release: " + c);
+
 	}
 
 }
