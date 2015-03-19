4db74bf953d8f68c97a347332cdaf6e9058eb147
unknown <xsb12164@CIS-ARROL-25.ds.strath.ac.uk>
2015-03-19 14:42:33 +0000

    Flipper added to master, Square collision fixed (mostly, kind of, maybe), Absorber fixed (again)
    by Craig, Alan and Sebastian

---
+++ gizmo/Absorber.java
@@ -0,0 +1,72 @@
+package model.gizmo;
+
+import java.awt.Color;
+
+import model.Ball;
+import model.Game;
+import model.Model;
+
+/**
+ * @author Grzegorz Sebastian Korkosz
+ */
+public class Absorber extends AbstractSquare {
+    private int absorbed;       // Number of absorbed balls. Not a boolean to support multiple balls.
+    private double velX;        // Starting X velocity of the ball spawned by this absorber
+    private double velY;        // Starting Y velocity of the ball spawned by this absorber
+    private Game parent;       // Parent board where the ball should be spawned
+    
+    
+    
+    /**
+     * Constructor for the Absorber
+     * @param x Position X on the board
+     * @param y Position Y on the board
+     * @param width Width in L. Has to be at least 1.
+     * @param height Height in L. Has to be at least 1.
+     * @param velX Starting X velocity of ball spawned by this absorber
+     * @param velY Starting Y velocity of ball spawned by this absorber
+     * @param parent Parent board containing the absorber
+     * @param id ID of the gizmo
+     */
+    public Absorber(String gizmoType, String id, int x, int y, int width, int height, Game parent) {
+    	super(gizmoType, id, x, y);
+    	this.parent = parent;
+        velX = 0;
+        velY = -50 * L;
+        this.absorbed = 0;
+        color = Color.MAGENTA;
+        System.out.println("width is " + width);
+        this.width = width;
+        this.height = height;
+        setPoints(x, y);
+        
+        System.out.println("width is " + this.width);
+
+        
+        this.addTrigger(this);  // TODO Remove this test method. At the moment it will trigger itself when hit.
+    }
+
+    /**
+     * Increments the number of absorbed balls.
+     */
+    public void absorb() {
+        absorbed++;
+    }
+    
+   
+    @Override
+    /**
+     * When absorber is triggered, it will spawn a ball in the bottom right corner
+     */
+    public void trigger() {
+        if (absorbed > 0) {
+            double ballX = botRightX - 0.5 * L; // X position of ball to be spawned by the absorber
+            double ballY = topLeftY - 0.25 * L; // Y position of ball to be spawned by the absorber
+            Ball ball = new Ball("", "", ballX, ballY, velX, velY);
+            ball.setIgnoreAbsorber(true);
+            parent.addBall(ball);
+            absorbed--;
+            // TODO Spawn the ball INSIDE of the absorber, not outside.
+        }
+    }
+}
---
+++ gizmo/AbstractFlipper.java
@@ -0,0 +1,138 @@
+package model.gizmo;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+import physics.Circle;
+import physics.LineSegment;
+
+/**
+ * Contains code that both flippers have in common
+ * @author Grzegorz Sebastian Korkosz
+ */
+public class AbstractFlipper extends AbstractGizmo {
+
+	private List<Gizmo> triggerList;
+	private double width = 12.5;
+	private double length = 37.5;
+	private int angle = 0;
+	private Color colour = Color.ORANGE;
+	private int x;
+	private int y;
+	private int x2;
+	private int y2;
+	private int L = 25;
+	private String gizmoType;
+	private ArrayList<Object> gizmoInfo;
+
+	public AbstractFlipper(String gizmoType, String id, int x, int y) {
+		super(gizmoType, id, x, y);
+		this.x = (x * L) + 6;
+		this.y = (y * L) + 6;
+		this.gizmoType = gizmoType;
+		getEndPoint();
+		gizmoInfo = new ArrayList<Object>();
+		gizmoInfo.add(gizmoType);
+		gizmoInfo.add(id);
+		gizmoInfo.add(x);
+		gizmoInfo.add(y);
+	}
+
+	public void setColour(){
+		colour = Color.ORANGE;
+	}
+
+	public void getEndPoint(){
+		double angleRad = angle * Math.PI / 180;
+		double yrad   = y + length * Math.cos(angleRad);
+		double xrad   = x + length * Math.sin(angleRad);
+		y2 = (int) yrad;
+		x2 = (int) xrad;
+	}
+	
+    public int getX(){
+    	return x;
+    }
+	
+    public int getY(){
+    	return y;
+    }
+
+	public int getx2(){
+		return x2;
+	}
+
+	public int gety2(){
+		return y2;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public double getLength(){
+		return length;
+	}
+
+	public int getAngle(){
+		return angle;
+	}
+	
+	public void setAngle(int a){
+		angle = a;
+	}
+
+	public Color getColour(){
+		return colour;
+	}
+
+	public void trigger(boolean t) {
+	} 
+
+	public List<Gizmo> getTriggers() {
+		List<Gizmo> triggerList = null;
+		return triggerList;
+	}
+	
+	public ArrayList<Object> getFlipperInfo(){
+		return gizmoInfo;
+	}
+
+	@Override
+	public String getType() {
+		return gizmoType;
+	}
+
+	@Override
+	public List<LineSegment> getSides() {
+		List<LineSegment> lineList = new ArrayList<LineSegment>();
+		LineSegment newls = new LineSegment(x+6.25, y+6.25, x2+6.25,y2);
+		lineList.add(newls);
+		LineSegment newls2 = new LineSegment(x-6.25, y-6.25, x2-6.25,y2);
+		lineList.add(newls2);
+		return lineList;
+	}
+
+	@Override
+	public List<Circle> getCorners() {
+		List<Circle> circleList = new ArrayList<Circle>();
+		Circle circle = new Circle(x-6,y-6,12);
+		circleList.add(circle);
+		Circle circle2 = new Circle(x2-6,y2-6,12);
+		circleList.add(circle2);
+		return circleList;
+	}
+
+	@Override
+	public int[] getAllXPos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[] getAllYPos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
