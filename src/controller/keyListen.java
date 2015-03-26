f909577a4197fba84fce40dd5d68ed18de63c3b9
cchalmers94 <craigch@sky.com>
2015-03-26 17:03:22 +0000

    Updated keyListen multiple
     Now allows multiple presses at once of key connects

--- src/controller/keyListen.java
+++ src/controller/keyListen.java
@@ -12,17 +12,18 @@ import model.gizmo.Gizmo;
 public class keyListen implements KeyListener {
 
 	private Board board;
-	private List<ArrayList<Object>> triggerKey;
+	private List<Integer> keysDown = new ArrayList<Integer>();
+	private List<Object> triggerKey;
 	private List<Integer> keys = new ArrayList<Integer>();
 	private List<String> gizmoID = new ArrayList<String>();
 	private List<String> direction = new ArrayList<String>();
 	private List<Gizmo> giz = new ArrayList<Gizmo>();;
 
-	public keyListen(Board board) {
-		this.board = board;
+	public keyListen(Board boar) {
+		board = boar;
 	}
 
-	
+
 	// keycode 81 = q
 	@Override
 	public void keyPressed(KeyEvent e) {
@@ -30,27 +31,31 @@ public class keyListen implements KeyListener {
 		gizmoID.clear();
 		direction.clear();
 		keys.clear();
-		for(ArrayList<Object> t: triggerKey){
-			System.out.println(t);
-			gizmoID.add((String) t.get(1));
-			direction.add((String) t.get(2));
-			keys.add((Integer) t.get(3));
+		for (int i = 0; i < triggerKey.size()-2; i= i +3){
+			gizmoID.add((String) triggerKey.get(i));
+			direction.add((String) triggerKey.get(i+1));
+			keys.add((Integer) triggerKey.get(i+2));
 		}
 		giz = board.getGizmoList();
 		int kID = e.getKeyCode();
-
+		if (!keysDown.contains(kID)){
+			keysDown.add(kID);
+		}
+		System.out.println(keysDown);
 		int index = 0;
-
-		if (keys.contains(kID)){
-			index = keys.indexOf(kID);
-			if (direction.get(index).equals("down")){
-				String gizID = gizmoID.get(index);
-				for (Gizmo gizmo : giz){
-					if (gizmo.getID().equals(gizID)){
-						if (gizmo.getType().equals("LeftFlipper") || gizmo.getType().equals("RightFlipper" )){
-							gizmo.trigger(true);
-						} else {
-							gizmo.trigger();
+		for (int k :keysDown){
+			System.out.println("keys "+ keys);
+			if (keys.contains(k)){
+				index = keys.indexOf(k);
+				if (direction.get(index).equals("down")){
+					String gizID = gizmoID.get(index);
+					for (Gizmo gizmo : giz){
+						if (gizmo.getID().equals(gizID)){
+							if (gizmo.getType().equals("LeftFlipper") || gizmo.getType().equals("RightFlipper" )){
+								gizmo.trigger(true);
+							} else {
+								gizmo.trigger();
+							}
 						}
 					}
 				}
@@ -64,34 +69,39 @@ public class keyListen implements KeyListener {
 		gizmoID.clear();
 		direction.clear();
 		keys.clear();
-		for(ArrayList<Object> t: triggerKey){
-			String dir = (String) t.get(2);
+		for (int i = 0; i < triggerKey.size()-2; i= i +3){
+			String dir = (String) triggerKey.get(i+1);
 			if (dir.equals("up")){
 				direction.add(dir);
-				gizmoID.add((String) t.get(1));
-				keys.add((Integer) t.get(3));
+				gizmoID.add((String) triggerKey.get(i));
+				keys.add((Integer) triggerKey.get(i+2));
+
 			}
 		}
 		giz = board.getGizmoList();
 		int kID = e.getKeyCode();
+		//keysDown.remove((Object)kID);
 
 		int index = 0;
-
-		if (keys.contains(kID)){
-			index = keys.indexOf(kID);
-			if (direction.get(index).equals("up")){
-				String gizID = gizmoID.get(index);
-				for (Gizmo gizmo : giz){
-					if (gizmo.getID().equals(gizID)){
-						if (gizmo.getType().equals("LeftFlipper") || gizmo.getType().equals("RightFlipper" )){
-							gizmo.trigger(false);
-						} else {
-							gizmo.trigger();
+		for (int k :keysDown){
+			if (keys.contains(k)){
+				index = keys.indexOf(kID);
+				if (direction.get(index).equals("up")){
+					String gizID = gizmoID.get(index);
+					for (Gizmo gizmo : giz){
+						if (gizmo.getID().equals(gizID)){
+							if (gizmo.getType().equals("LeftFlipper") || gizmo.getType().equals("RightFlipper" )){
+								gizmo.trigger(false);
+							} else {
+								gizmo.trigger();
+							}
 						}
 					}
 				}
 			}
 		}
+		keysDown.remove((Object)kID);
+
 	}
 	@Override
 	public void keyTyped(KeyEvent e) {
