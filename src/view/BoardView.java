704bd92c8b50ba532b7c193ea37d8827ed896f02
alankm194 <ekb12168@uni.strath.ac.uk>
2015-04-01 01:03:55 +0100

    -Fixed the ball falling through gizmos and walls. -fixed weird ball collisions with rows of square gizmos. -changed size of ball and positioned the ball to be in the middle of the grid when created.

--- src/view/BoardView.java
+++ src/view/BoardView.java
@@ -68,8 +68,8 @@ public  class BoardView extends JPanel implements Observer {
 			Ball b = ballList.get(i);
 			if (b != null) {
 				g2.setColor(b.getColour());
-				int x = (int) (b.getExactX());
-				int y = (int) (b.getExactY());
+				int x = (int) (b.getExactX() - b.getRadius());
+				int y = (int) (b.getExactY() - b.getRadius());
 				int width = (int) (2 * b.getRadius());
 				g2.fillOval(x, y, width, width);
 			}
